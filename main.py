00028a6aef8390393974b655fd5b873c7efb90f9
Beringei0 <asksnyta@hotmail.com>
2023-01-11 22:23:55 +0100

    update

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import sqlite3 as sq
 import pandas as pd 
-#------------------all imported modules
 
 
-def main():
 
-    conn = sq.connect('test.db')
-    c = conn.cursor()
-    data = pd.read_csv('personer.csv')
-    data.to_sql('kunder', conn, if_exists= 'replace', index=False)
-    c.execute('SELECT * FROM kunder')
-    print(c.fetchall())
-    conn.commit()
-    conn.close()
+conn = sq.connect('test.db')
 
-main()
-#---------------------main function 
+c = conn.cursor()
+
+table =('CREATE TABLE IF NOT EXISTS kundeTable (kundenr TEXT PRIMARY KEY, fname TEXT, ename TEXT, tlf INTEGER, epost TEXT, postnummer INTEGER)')
+
+data = pd.read_csv('personer.csv')
+
+c.execute(table)
+
+data.to_sql('kundeTable', conn, if_exists ='append', index=False)
+
+for row in c.execute('SELECT * FROM kundeTable'):
+    print (row)
+
+print(c.fetchall())
+
+conn.close()
+
+
+
