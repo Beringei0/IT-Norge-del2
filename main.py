0e79d976808b9884958046649402b0366525a995
Beringei0 <asksnyta@hotmail.com>
2023-01-16 11:55:55 +0100

    update

--- main.py
+++ main.py
@@ -17,25 +17,18 @@ def main():
 def createtable():
 
     #creates a new table with column "kundenr" as primary
-    table =('CREATE TABLE IF NOT EXISTS kundeTable (kundenr TEXT PRIMARY KEY, fname TEXT, ename TEXT, tlf INTEGER, postnummer INTEGER)') 
+    table =('CREATE TABLE IF NOT EXISTS kundeTable (kundenr TEXT PRIMARY KEY, fname TEXT, ename TEXT, epost TEXT, tlf INTEGER, postnummer INTEGER)') 
     
     c.execute(table) #import table into DB
 
     
 #---------------function that loads in data from csv file 
 def load_data():
-    try: # tryblock rturns a value everytime the code runs into an exception or not
 
         data = pd.read_csv('personer.csv') #reads in csv file
-        data.to_sql('kundeTable', conn, if_exists ='replace', index=False) #sends data in sql format into "kundeTable"
+        data.to_sql('kundeTable', conn, if_exists ='append', index=False) #sends data in sql format into "kundeTable"
 
-        for row in c.execute('SELECT * FROM kundeTable'): #loop that returns ever row in the created table 
-           print(row) #prints every single row in DB
-
-    except: #returns when code runs into an exception
-        print("Something went wrong while importing data!")
-    finally: #returns when code runs without excpetions 
-        print("Data has been imported!")
+    
 
 
 
